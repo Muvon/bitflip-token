198f9f618de44622c52f58d9eede0caa3bf43043
Dmitry Kuzmenkov <dmitry@wagh.ru>
2017-11-03 20:52:15 +0700

    Change ticker from LTC to FLIP in src dir

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("FLIP");
+    case mBTC: return QString("mFLIP");
+    case uBTC: return QString::fromUtf8("μFLIP");
     default: return QString("???");
     }
 }
