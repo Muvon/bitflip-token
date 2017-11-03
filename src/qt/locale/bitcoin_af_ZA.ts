3a19b63d9142deb56881c73dae73ab2f4ce98dc2
Dmitry Kuzmenkov <dmitry@wagh.ru>
2017-11-03 20:51:23 +0700

    Change all names in src dir from litecoin -> bitflip

--- src/qt/locale/bitcoin_af_ZA.ts
+++ src/qt/locale/bitcoin_af_ZA.ts
@@ -214,8 +214,8 @@
         <translation>&amp;Opsies</translation>
     </message>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>BitFlip</source>
+        <translation>BitFlip</translation>
     </message>
     <message>
         <source>Wallet</source>
--- src/qt/locale/bitcoin_et_EE.ts
+++ src/qt/locale/bitcoin_et_EE.ts
@@ -130,8 +130,8 @@
         <translation>Kõvakettal olevate plokkide reindekseerimine...</translation>
     </message>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>BitFlip</source>
+        <translation>BitFlip</translation>
     </message>
     <message>
         <source>Wallet</source>
@@ -760,8 +760,8 @@
         <translation>Valikud:</translation>
     </message>
     <message>
-        <source>Litecoin Core</source>
-        <translation>Litecoin Core</translation>
+        <source>BitFlip Core</source>
+        <translation>BitFlip Core</translation>
     </message>
     <message>
         <source>Information</source>
