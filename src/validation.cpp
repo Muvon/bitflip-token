3034fd72d30f2f5a3171ed0af91906188ea18765
Dmitry Kuzmenkov <dmitry@wagh.ru>
2017-11-05 07:42:05 +0700

    Add premine amount for first block

--- src/validation.cpp
+++ src/validation.cpp
@@ -1170,7 +1170,13 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    CAmount nSubsidy = 50 * COIN;
+    
+    if(nHeight == 1)
+    {
+        CAmount nSubsidy = 10000000 * COIN;
+    } else {
+        CAmount nSubsidy = 50 * COIN;
+    }
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
